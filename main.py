35104086e481684c9f6438bd3af6f9bae28baebc
Yash Verma <yashvrx1@gmail.com>
2023-07-27 16:21:44 +0530

    Project 1 is done.

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
 print("Git Checking")
-<<<<<<< HEAD
-=======
-print("Day 1 - Python Print Function")
-print("The function is declared like this:")
-print("print('what to print')")
->>>>>>> 0bd5c20 (This is initial Stage for our Challenge.)
 
 
