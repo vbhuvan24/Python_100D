012d9d1f8266db88fff2ec30e28fe4abbfb66284
Yash Verma <yashvrx1@gmail.com>
2023-07-28 22:21:15 +0530

    Project 2 in progress.

---
+++ Greet.py
@@ -0,0 +1,19 @@
+#Simple Function
+# def Greet():
+#     print("Ram Ram Uncle ji\nRam Ram Uncle ji\nRam Ram Uncle ji")
+#
+# Greet()
+
+#Simple Function with input
+
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print(f"How do you do {name}")
+#
+# greet_with_name("Yash Verma")
+
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}")
+
+greet_with(location = "Meerut", name = "Yash")
